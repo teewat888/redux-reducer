fdca14631c60c45f185b9b67f15c8239d244055e
TEERAWAT PRAROM <teewat@yahoo.com>
2022-02-23 23:03:27 +1100

    completed assignment-refatcot

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,18 +1,16 @@
 export function manageFriends(state = { friends: [] }, action) {
-  let current_state;
   switch (action.type) {
     case "ADD_FRIEND":
-      current_state = { ...state };
-      current_state.friends = [...current_state.friends];
-      current_state.friends.push(action.friend);
-      return current_state;
+      return {
+        ...state,
+        friends: [...state.friends, action.friend],
+      };
       break;
     case "REMOVE_FRIEND":
-      current_state = { ...state };
-      current_state.friends = [...current_state.friends].filter(
-        (f) => f.id !== action.id
-      );
-      return current_state;
+      return {
+        ...state,
+        friends: [...state.friends].filter((f) => f.id != action.id),
+      };
       break;
     default:
       return state;
